ce5d42c962346b18cd2dfad6794aa665d6d66311
JBitar <JBitar@anaheim.net>
2024-04-30 10:49:30 -0700

    Added Inventory view and model

---
+++ Models/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GUN_RANGE_APP_V5.Models
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string OfficerId { get; set; } = string.Empty;
+        public string GunInformation { get; set; } = string.Empty;
+        public string AdditionalEquipment { get; set; } = string.Empty;
+                       
+    }
+}
---
+++ Models/InventoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GUN_RANGE_APP_V5.Models
+{
+    public class InventoryViewModel
+    {
+        public InventoryViewModel()
+        {
+            InventoryItems = new List<Inventory>();
+        }
+        public List<Inventory> InventoryItems { get; set; }
+    }
+}
+
